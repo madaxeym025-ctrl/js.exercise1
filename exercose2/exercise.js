0a188058241fcc9db5068ca64d966fc2c2d1a563
madaxeym025-ctrl <madaxeym025@gmail.com>
2026-06-07 10:10:40 +0300

    exercise3

---
+++ exercose2/exercise.js
@@ -0,0 +1,11 @@
+let x = 15;
+let y = "15";
+
+console.log(x == y);
+console.log(x != y);
+console.log(x === y);
+console.log(x !== y);
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
