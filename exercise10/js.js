f0d33881ef9e70195cdd78a9f2379e02fe871a13
madaxeym025-ctrl <madaxeym025@gmail.com>
2026-06-09 22:48:04 +0300

    file cusub

---
+++ exercise10/js.js
@@ -0,0 +1,19 @@
+let libarya= [
+    {
+        name:"Hawale waran",
+        aothury:"tariq iyo wacyi galin",
+        year:"2012"
+    },
+    {
+        name:"Tariqda Hore",
+        aothury:"tariq iyo wacyi galin",
+        year:"2018"
+    },
+    {
+        name:"Manta iyo dunida",
+        aothury:"tariq iyo wacyi galin",
+        year:"2026"
+    }
+
+]
+console.log(libarya)
