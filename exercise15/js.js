45ad41df4ec1e82eef30ed21700e24992a0953b9
madaxeym025-ctrl <madaxeym025@gmail.com>
2026-06-23 21:21:40 +0300

    xercise15

---
+++ exercise15/js.js
@@ -0,0 +1,23 @@
+let xog = [
+    {
+        name:"maxamed",
+        age:19,
+        career:"software developer",
+    },
+    {
+        name:"yusuf",
+        age:19,
+        career:"software developer",
+    },
+    {
+        name:"xamdi",
+        age:19,
+        career:"software developer",
+    }
+]
+    
+
+for(let xogo in xog){
+    console.log(xog[xogo])
+}
+    
