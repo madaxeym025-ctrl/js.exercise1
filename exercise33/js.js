c76b5d75f24994b34a9449a7a25532f613092651
madaxeym025-ctrl <madaxeym025@gmail.com>
2026-06-26 21:37:24 +0300

    exercise33

---
+++ exercise33/js.js
@@ -0,0 +1,17 @@
+let name = document.querySelector('.my')
+let hero = document.querySelector(".hero")
+let change = document.querySelector(".ch")
+let reback = document.querySelector(".re")
+
+change.addEventListener("click",()=>{
+    name.innerHTML = "madaxey"
+})
+
+reback.addEventListener("click",()=>{
+    hero.innerHTML = "idol"
+})
+
+console.log("hi")
+
+
+
